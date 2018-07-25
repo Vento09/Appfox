ca9d8a7119b4344d7fc43d672c0bdea012b555ee
Vento09 <VictoryLose@gmail.com>
2018-07-25 19:27:11 -0300

    Add files via upload

---
+++ Appfox.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Let's start!"
+
+cp Principal.desktop $1.desktop
+
+echo "Configuring the application"
+
+echo "Name=$1" >> $1.desktop
+echo "Exec=firefox -P app $2" >> $1.desktop
+echo "Icon=$3" >> $1.desktop
+
+mv $1.desktop ~/.local/share/applications
---
+++ delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm ~/.local/share/applications/$1.desktop
