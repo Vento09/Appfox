b9d793536b6125a5ca91a3dc5f12b0e2b272f42b
Vento09 <VictoryLose@gmail.com>
2018-07-25 23:53:07 -0300

    Creating profile by itself
    Now Appfox create the profile by itself and it ask for the name, url and the icon

--- Appfox.sh
+++ Appfox.sh
@@ -2,14 +2,46 @@
 
 echo "Let's start!"
 
-cp Principal.desktop $1.desktop
+# Questions
+
+cat Witn.txt
+
+echo "Put the next number bellow"
+read num
+
+# Changing the archive What is the number 
+echo "$num" > Witn.txt
+
+echo "What is the name of the WebApp?"
+read name
+
+echo "What is the URL?"
+read url
+
+echo "Where is the icon? (or) What is the icon name?"
+read icon
 
 echo "Configuring the application"
 
-echo "Name=$1" >> $1.desktop
-echo "Exec=firefox -P app $2" >> $1.desktop
-echo "Icon=$3" >> $1.desktop
+# Creating the Firefox profile 
 
-mv $1.desktop ~/.local/share/applications
+mkdir ~/.mozilla/firefox/appfox."$name"
 
-echo "All done"
+echo "[Profile${num}]" >> ~/.mozilla/firefox/profiles.ini
+echo "Name=$name" >> ~/.mozilla/firefox/profiles.ini
+echo "IsRelative=1" >> ~/.mozilla/firefox/profiles.ini
+echo "Path=appfox.$name" >> ~/.mozilla/firefox/profiles.ini
+
+mkdir ~/.mozilla/firefox/appfox."$name"/chrome 
+cp userChrome.css ~/.mozilla/firefox/appfox."$name"/chrome
+
+# Creating the launcher 
+cp Principal.desktop "$name".desktop
+
+echo "Name=$name" >> "$name".desktop
+echo "Exec=firefox -P "$name" $url" >> "$name".desktop
+echo "Icon=$icon" >> "$name".desktop
+
+mv "$name".desktop ~/.local/share/applications
+
+echo "All done. Enjoy with it!"
